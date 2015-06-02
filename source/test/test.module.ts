bb076907e913a63ecb1eadc26e59952821dc339f
Josh Graber <jgraber@renovo1.com>
2015-06-02 11:05:05 -0400

    Add a module for test utilities and include this module in the main utilities module

---
+++ source/test/test.module.ts
@@ -0,0 +1,7 @@
+import __angularFixture = require('./angularFixture');
+import __mock = require('./mock');
+
+/* tslint:disable */
+// reference components and services so that both are included as children of this page
+[__angularFixture, __mock];
+/* tslint:enable */
--- source/utilities.ts
+++ source/utilities.ts
@@ -2,20 +2,27 @@
 
 export var name: string = 'rl.utilities';
 
-import __observable = require('./observable/observable.module');
-import __jquery = require('./jquery/jquery.module');
-import __number = require('./number/number.module');
-import __promise = require('./promise/promise.module');
 import __array = require('./array/array.module');
 import __date = require('./date/date.module');
+import __jquery = require('./jquery/jquery.module');
+import __number = require('./number/number.module');
 import __object = require('./object/object.module');
+import __observable = require('./observable/observable.module');
+import __promise = require('./promise/promise.module');
 
 angular.module(name, [
-	__observable.name,
-	__jquery.name,
-	__number.name,
-	__promise.name,
 	__array.name,
 	__date.name,
+	__jquery.name,
+	__number.name,
 	__object.name,
+	__observable.name,
+	__promise.name,
 ]);
+
+import __test = require('./test/test.module');
+
+/* tslint:disable */
+// reference components and services so that both are included as children of this page
+[__test];
+/* tslint:enable */
