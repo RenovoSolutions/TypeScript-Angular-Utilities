4a414c7a2a9656175adf428840d68b0d8b8acbb8
Josh Graber <jgraber@renovo1.com>
2015-06-02 11:04:05 -0400

    Remove gulp folder and use gulp-utilities instead.

--- gulp/lint.js
+++
@@ -1,9 +0,0 @@
-var tslint = require('gulp-tslint');
-
-exports.config = function(gulp) {
-    gulp.task('lint', function() {
-		gulp.src(['./source/**/*.ts', '!./source/typings/*.d.ts'])
-			.pipe(tslint())
-			.pipe(tslint.report('verbose'));
-    });
-};
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,58 @@
-/// <vs BeforeBuild='build' AfterBuild='resolve' Clean='clean' />
+/// <vs BeforeBuild='build' Clean='clean' />
 var gulp = require('gulp');
-var build = require('./gulp/build.js');
-var test = require('./gulp/test.js');
+var lint = require('./libraries/gulp-utilities/lint');
+var clean = require('./libraries/gulp-utilities/clean');
+var copy = require('./libraries/gulp-utilities/copy');
+var test = require('./libraries/gulp-utilities/test');
 
-build.config(gulp);
-test.config(gulp);
+lint.config(gulp);
+clean.config(gulp);
+copy.config(gulp);
+test.config(gulp, __dirname + '/karma.conf.js');
 
 gulp.task('default', ['build']);
 
 var runSequence = require('run-sequence');
 
 gulp.task('tc', function(done) {
-    runSequence('build.release',
-                'test.tc',
-                done);
-});
-
-gulp.task('run', function(done) {
-    runSequence('build.release',
-                'serve',
-                done);
+	runSequence('build.release',
+				'test.tc',
+				done);
 });
 
 gulp.task('run.tests', function (done) {
 	runSequence('build.release',
-                'test',
-                done);
+				'test',
+				done);
+});
+
+gulp.task('build', ['build.debug']);
+
+gulp.task('build.debug', function(done) {
+	runSequence('lint',
+				'clean.debug',
+				'compile.debug',
+				'copy.debug',
+				done);
+});
+
+gulp.task('build.release', function(done) {
+	runSequence('lint',
+				'clean.release',
+				'compile.release',
+				'copy.release',
+				done);
+});
+
+var browserify = require('./libraries/gulp-utilities/browserify');
+var main = 'utilities'; 
+
+gulp.task('compile', ['compile.debug']);
+
+gulp.task('compile.debug', function(done) {
+	return browserify.compileDebug(main);
+});
+
+gulp.task('compile.release', function(done) {
+	return browserify.compileRelease(main);
 });
