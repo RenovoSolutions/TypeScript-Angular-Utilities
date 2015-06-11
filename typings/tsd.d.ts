f68ef0309a292bf2f7e1274536df386db55fe104
Josh Graber <jgraber@renovo1.com>
2015-06-11 16:12:11 -0400

    Add global references to tsd.d.ts so that the problemWatcher will recognize them.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="moment/moment-node.d.ts" />
+
+/// <reference path='angularjs/angular.d.ts' />
+/// <reference path='lodash/lodash.d.ts' />
+/// <reference path='jquery/jquery.d.ts' />
