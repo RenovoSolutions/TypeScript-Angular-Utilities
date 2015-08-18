01dc50533909e39b53eee80d6ea69bc3fd0b3bf0
Josh Graber <jgraber@renovo1.com>
2015-08-18 10:11:14 -0400

    Update to gulp-utilities 0.10.8.

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,9 @@
 var libraries = require('./libReferences.json');
 var defaultKarma = require('gulp-utilities').karma;
 
+var locations = require('./locations.json');
+
 module.exports = function(karma) {
-	config = defaultKarma(karma, libraries);
-	karma.set(config)
+	config = defaultKarma(karma, libraries, locations);
+	karma.set(config);
 };
