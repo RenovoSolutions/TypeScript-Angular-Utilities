ce30243f613a6b7fe69fe29d2db41b6bca97cfa6
unknown <csengineer13@gmail.com>
2015-08-18 14:33:26 -0400

    Updating 'parentChildBehavior' utility to match RL21

--- source/services/parentChildBehavior/parentChildBehavior.service.tests.ts
+++ source/services/parentChildBehavior/parentChildBehavior.service.tests.ts
@@ -77,5 +77,52 @@ module rl.utilities.services.parentChildBehavior {
 				expect(behaviors[1]).to.equal(behavior2);
 			});
 		});
+
+		describe('triggerChildBehavior', (): void => {
+			it('should trigger the specified child action and return the result', (): void => {
+				var behavior1: ITestBehavior = { action: (): number => { return 3; } };
+				var child: IChild<ITestBehavior> = { viewData: { behavior: behavior1 } };
+
+				var behaviorResult: number = parentChildBehavior.triggerChildBehavior(child,
+					(behavior: ITestBehavior): number => {
+					return behavior.action();
+				});
+
+				expect(behaviorResult).to.equal(3);
+			});
+
+			it('should return null if the behavior does not exist', (): void => {
+				var child: IChild<ITestBehavior> = { };
+
+				var behaviorResult: number = parentChildBehavior.triggerChildBehavior(child,
+					(behavior: ITestBehavior): number => {
+					return behavior.action();
+				});
+
+				expect(behaviorResult).to.be.null;
+			});
+		});
+
+		describe('triggerAllChildBehaviors', (): void => {
+			it('should trigger the specified child actions and return the results', (): void => {
+				var behavior1: ITestBehavior = { action: (): number => { return 1; } };
+				var child1: IChild<ITestBehavior> = { viewData: { behavior: behavior1 } };
+				var behavior2: ITestBehavior = { action: (): number => { return 2; } };
+				var child2: IChild<ITestBehavior> = { viewData: { behavior: behavior2 } };
+				var behavior3: ITestBehavior = { action: (): number => { return 3; } };
+				var child3: IChild<ITestBehavior> = { viewData: { behavior: behavior3 } };
+				var childWithoutBehavior: IChild<ITestBehavior> = { };
+
+				var behaviorResult: number[] = parentChildBehavior.triggerAllChildBehaviors([child1, child2, child3, childWithoutBehavior],
+					(behavior: ITestBehavior): number => {
+					return behavior.action();
+				});
+
+				expect(behaviorResult).to.have.length(3);
+				expect(behaviorResult[0]).to.equal(1);
+				expect(behaviorResult[1]).to.equal(2);
+				expect(behaviorResult[2]).to.equal(3);
+			});
+		});
 	});
 }
