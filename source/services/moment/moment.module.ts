95b8e198c021dac2773c9e104827074d115aecb1
unknown <csengineer13@gmail.com>
2015-08-26 11:21:04 -0400

    Adding moment wrapper

---
+++ source/services/moment/moment.module.ts
@@ -0,0 +1,25 @@
+module rl.utilities.services.moment {
+	export var moduleName: string = 'rl.utilities.services.moment';
+	export var serviceName: string = 'moment';
+
+	export function momentWrapper(): void {
+		'use strict';
+		// Using `any` instead of MomentStatic because
+		//  createFromInputFallback doesn't appear to be
+		//  defined in MomentStatic... :-(
+		var moment: any = (<any> window).moment; // moment must already be loaded
+
+		// Set default method for handling non-ISO date conversions.
+		// See 4/28 comment in https://github.com/moment/moment/issues/1407
+		// This also prevents the deprecation warning message to the console.
+		moment.createFromInputFallback = (config: any): void => {
+			config._d = new Date(config._i);
+		};
+
+		return moment;
+	}
+
+	angular.module(moduleName, [])
+	.factory(serviceName, momentWrapper);
+
+}
