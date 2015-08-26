82b9cdbaf56c7e4e989422b1df2ee7c8f7e60afa
unknown <csengineer13@gmail.com>
2015-08-26 13:59:33 -0400

    Fixing failing moment test in constructor

--- source/services/moment/moment.module.ts
+++ source/services/moment/moment.module.ts
@@ -6,19 +6,20 @@ module rl.utilities.services.momentWrapper {
 
 	export function momentWrapper(): void {
 		'use strict';
+
 		// Using `any` instead of MomentStatic because
 		//  createFromInputFallback doesn't appear to be
 		//  defined in MomentStatic... :-(
-		var moment: any = (<any> window).moment; // moment must already be loaded
+		var momentWrapper: any = moment; // moment must already be loaded
 
 		// Set default method for handling non-ISO date conversions.
 		// See 4/28 comment in https://github.com/moment/moment/issues/1407
 		// This also prevents the deprecation warning message to the console.
-		moment.createFromInputFallback = (config: any): void => {
+		momentWrapper.createFromInputFallback = (config: any): void => {
 			config._d = new Date(config._i);
 		};
 
-		return moment;
+		return momentWrapper;
 	}
 
 	angular.module(moduleName, [])
