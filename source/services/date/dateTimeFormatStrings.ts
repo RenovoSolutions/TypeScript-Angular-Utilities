87df19ff6a721ae93d2c5b13e5f3b7919d836bb9
Josh Graber <jgraber@renovo1.com>
2015-09-24 11:17:57 -0400

    Convert date, moment, time, and compareResult to ES6 syntax

--- source/services/date/dateTimeFormatStrings.ts
+++ source/services/date/dateTimeFormatStrings.ts
@@ -1,16 +1,15 @@
+'use strict';
 
-module rl.utilities.services.date {
-	export var dateTimeFormatServiceName: string = 'dateTimeFormatStrings';
+export var dateTimeFormatServiceName: string = 'dateTimeFormatStrings';
 
-	export interface IDateFormatStrings {
-		dateTimeFormat: string;
-		dateFormat: string;
-		timeFormat: string;
-	}
-
-	export var defaultFormats: IDateFormatStrings = {
-		dateTimeFormat: 'M/D/YYYY h:mm A',
-		dateFormat: 'M/D/YYYY',
-		timeFormat: 'h:mmA',
-	};
+export interface IDateFormatStrings {
+	dateTimeFormat: string;
+	dateFormat: string;
+	timeFormat: string;
 }
+
+export var defaultFormats: IDateFormatStrings = {
+	dateTimeFormat: 'M/D/YYYY h:mm A',
+	dateFormat: 'M/D/YYYY',
+	timeFormat: 'h:mmA',
+};
