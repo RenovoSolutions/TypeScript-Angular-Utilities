4db4fcbe41c454dc9e5d60fb0684659cf8979f8e
Josh Graber <jgraber@renovo1.com>
2015-09-24 11:30:24 -0400

    Add migrated components to the services module.

--- source/services/services.module.ts
+++ source/services/services.module.ts
@@ -3,7 +3,15 @@
 import * as angular from 'angular';
 
 import * as array from './array/array.service';
+import * as autosave from './autosave/autosave.service';
+import * as autosaveAction from './autosaveAction/autosaveAction.service';
+import * as boolean from './boolean/boolean.service';
+import * as date from './date/date.module';
+
+import * as moment from './moment/moment.module';
 import * as objectService from './object/object.service';
+import * as observable from './observable/observable.service';
+import * as time from './time/time.service';
 
 // /// <reference path='array/array.service.ts' />
 // /// <reference path='autosave/autosave.service.ts' />
@@ -25,19 +33,33 @@ import * as objectService from './object/object.service';
 // /// <reference path='time/time.service.ts' />
 // /// <reference path='validation/validation.service.ts' />
 
-export { array, objectService };
+export {
+	array,
+	autosave,
+	autosaveAction,
+	boolean,
+	date,
+
+	moment,
+	objectService as object,
+	observable,
+	time,
+};
 
 export var name: string = 'rl.utilities.services';
 
 angular.module(name, [
 	array.moduleName,
+	autosave.moduleName,
+	autosaveAction.moduleName,
+	boolean.moduleName,
+	date.moduleName,
+
+	moment.moduleName,
 	objectService.moduleName,
-	// array.moduleName,
-	// autosave.moduleName,
-	// autosaveAction.moduleName,
-	// boolean.moduleName,
-	// contentProvider.moduleName,
-	// date.moduleName,
+	observable.moduleName,
+	time.moduleName,
+
 	// fileSize.moduleName,
 	// genericSearchFilter.moduleName,
 	// jquery.moduleName,
