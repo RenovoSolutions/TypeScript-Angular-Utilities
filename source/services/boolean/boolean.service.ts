f0cb406d5bda04e1779621a68042e17c631d4b75
Josh Graber <jgraber@renovo1.com>
2015-09-24 10:32:56 -0400

    Covnert boolean service to ES6 syntax

--- source/services/boolean/boolean.service.ts
+++ source/services/boolean/boolean.service.ts
@@ -1,21 +1,19 @@
-// uses typings/angular
+'use strict';
 
-module rl.utilities.services.boolean {
-	'use strict';
+import * as angular from 'angular';
 
-	export var moduleName: string = 'rl.utilities.services.boolean';
-	export var serviceName: string = 'booleanUtility';
+export var moduleName: string = 'rl.utilities.services.boolean';
+export var serviceName: string = 'booleanUtility';
 
-	export interface IBooleanUtility {
-		toBool(object: any): boolean;
-	}
-
-	class BooleanUtility implements IBooleanUtility {
-		toBool(object: any): boolean {
-			return !!object;
-		}
-	}
-
-	angular.module(moduleName, [])
-		.service(serviceName, BooleanUtility);
+export interface IBooleanUtility {
+	toBool(object: any): boolean;
 }
+
+class BooleanUtility implements IBooleanUtility {
+	toBool(object: any): boolean {
+		return !!object;
+	}
+}
+
+angular.module(moduleName, [])
+	.service(serviceName, BooleanUtility);
