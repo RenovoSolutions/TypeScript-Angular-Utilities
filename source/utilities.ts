6c8c424f152f16a9798416e1820d93574d40606f
Josh Graber <jgraber@renovo1.com>
2015-09-28 11:44:16 -0400

    The type definitions need to be publicly visible (exported) as well.

--- source/utilities.ts
+++ source/utilities.ts
@@ -5,8 +5,9 @@ import * as angular from 'angular';
 import * as behaviors from './behaviors/behaviors.module';
 import * as filters from './filters/filters.module';
 import * as services from './services/services.module';
+import * as types from './types/types.module';
 
-export { behaviors, filters, services };
+export { behaviors, filters, services, types };
 
 export var name: string = 'rl.utilities';
 
