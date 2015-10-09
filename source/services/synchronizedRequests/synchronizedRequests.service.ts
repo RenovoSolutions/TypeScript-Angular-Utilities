fd9449f08de2bbdc2592a9ae6a8b39ae7f98bce0
SamGraber <sgraber@renovo1.com>
2015-10-09 09:42:24 -0400

    Actually we need to expose a factory interface, not a service.

--- source/services/synchronizedRequests/synchronizedRequests.service.ts
+++ source/services/synchronizedRequests/synchronizedRequests.service.ts
@@ -3,11 +3,35 @@
 import * as angular from 'angular';
 
 export var moduleName: string = 'rl.utilities.services.synchronizedRequests';
-export var serviceName: string = 'synchronizedRequests';
+export var factoryName: string = 'synchronizedRequests';
+
+export interface ISynchronizedRequestsService {
+	// interface
+}
 
 export class SynchronizedRequestsService {
 	// implementation
 }
 
+export interface IRequestGetter {
+	(...params: any[]): angular.IPromise<any>;
+}
+
+export interface IRequestCallback {
+	(...data: any[]): void;
+}
+
+export interface ISynchronizedRequestsFactory {
+	getInstance(getData: IRequestGetter, callback: IRequestCallback): ISynchronizedRequestsService;
+}
+
+export function synchronizedRequestsFactory(): ISynchronizedRequestsFactory {
+	return {
+		getInstance(getData: IRequestGetter, callback: IRequestCallback): ISynchronizedRequestsService {
+			return new SynchronizedRequestsService();
+		},
+	};
+}
+
 angular.module(moduleName, [])
-	.service(serviceName, SynchronizedRequestsService);
+	.factory(factoryName, synchronizedRequestsFactory);
