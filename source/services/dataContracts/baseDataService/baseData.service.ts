9db545431cfe0b375fb527699377be1e27cf8772
SamGraber <sgraber@renovo1.com>
2015-10-16 12:06:46 -0400

    Use the existing base data service for the contracts functionality (We'll make slight changes as necessary, and then discard the old data service, as this is a copy for now)

--- source/services/dataContracts/baseDataService/baseData.service.ts
+++ source/services/dataContracts/baseDataService/baseData.service.ts
@@ -1,13 +1,77 @@
-import * as ng from 'angular';
+'use strict';
+
+import * as angular from 'angular';
+
+export var moduleName: string = 'rl.utilities.services.baseDataService';
+export var factoryName: string = 'baseDataService';
 
 export interface IBaseDomainObject {
     id: number;
 }
 
 export interface IBaseDataService<TDataType extends IBaseDomainObject, TSearchParams> {
-	getList(params?: TSearchParams): ng.IPromise<TDataType[]>;
-    getDetail(id: number): ng.IPromise<TDataType>;
-    post(domainObject: TDataType): ng.IPromise<TDataType>;
-    put(domainObject: TDataType): ng.IPromise<void>;
-    delete(id: number): ng.IPromise<void>;
+	getList(params?: TSearchParams): angular.IPromise<TDataType[]>;
+    getDetail(id: number): angular.IPromise<TDataType>;
+    post(domainObject: TDataType): angular.IPromise<TDataType>;
+    put(domainObject: TDataType): angular.IPromise<void>;
+    delete(id: number): angular.IPromise<void>;
 }
+
+export class BaseDataService<TDataType extends IBaseDomainObject, TSearchParams> implements IBaseDataService<TDataType, TSearchParams> {
+    constructor(private $http: angular.IHttpService, private endpoint: string) { }
+
+    // Build request URL
+    private getEndpoint(): string {
+        return this.endpoint;
+    }
+
+    // All
+    getList(): angular.IPromise<TDataType[]> {
+        return this.$http.get(this.getEndpoint())
+            .then((response: angular.IHttpPromiseCallbackArg<TDataType[]>): TDataType[]=> {
+                return response.data;
+            });
+    }
+
+    // Single
+    getDetail(id: Number): angular.IPromise<TDataType> {
+        return this.$http.get(this.getEndpoint() + '/' + id.toString())
+            .then((response: angular.IHttpPromiseCallbackArg<TDataType>): TDataType=> {
+                return response.data;
+            });
+    }
+
+    // Update
+    post(domainObject: TDataType): angular.IPromise<TDataType> {
+        return this.$http.post(this.getEndpoint(), JSON.stringify(domainObject))
+            .then((result: angular.IHttpPromiseCallbackArg<TDataType>): TDataType => {
+                return result.data;
+            });
+    }
+
+    // Create
+    put(domainObject: TDataType): angular.IPromise<void> {
+        return this.$http.put<void>(this.getEndpoint(), domainObject).then((): void => { return null; });
+    }
+
+    // Delete
+    delete(id: number): angular.IPromise<void> {
+        return this.$http.delete<void>(this.getEndpoint() + '/' + id.toString()).then((): void => { return null; });
+    }
+}
+
+export interface IBaseDataServiceFactory {
+    getInstance<TDataType extends IBaseDomainObject, TSearchParams>(endpoint: string): IBaseDataService<TDataType, TSearchParams>;
+}
+
+baseDataServiceFactory.$inject = ['$http'];
+export function baseDataServiceFactory($http: angular.IHttpService): IBaseDataServiceFactory {
+    return {
+        getInstance<TDataType extends IBaseDomainObject, TSearchParams>(endpoint: string): IBaseDataService<TDataType, TSearchParams> {
+            return new BaseDataService<TDataType, TSearchParams>($http, endpoint);
+        },
+    };
+}
+
+angular.module(moduleName, [])
+    .factory(factoryName, baseDataServiceFactory);
