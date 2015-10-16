e316d50b4adcb6fc5792ee553cf283c6e44a706a
SamGraber <sgraber@renovo1.com>
2015-10-16 13:44:05 -0400

    Don't require $http to be passed in. Since the baseResourceBuilder is a service, we can inject it here and pass it in. Also inject $q, as we'll need it for the mocks.

--- source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
+++ source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
@@ -9,22 +9,25 @@ export var moduleName: string = 'rl.utilities.services.baseResourceBuilder';
 export var serviceName: string = 'baseResourceBuilder';
 
 export interface IBaseResourceBuilder {
-	createResource<TDataType extends IBaseDomainObject, TSearchParams>($http: angular.IHttpService, endpoint: string, mockData: TDataType[], useMock: boolean): IBaseDataService<TDataType, TSearchParams>;
-	createResource<TDataType extends IBaseDomainObject>($http: angular.IHttpService, endpoint: string, mockData: TDataType[], useMock: boolean): IBaseDataService<TDataType, void>;
+	createResource<TDataType extends IBaseDomainObject, TSearchParams>(endpoint: string, mockData: TDataType[], useMock: boolean): IBaseDataService<TDataType, TSearchParams>;
+	createResource<TDataType extends IBaseDomainObject>(endpoint: string, mockData: TDataType[], useMock: boolean): IBaseDataService<TDataType, void>;
 	createParentResource<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
-		($http: angular.IHttpService, endpoint: string, mockData: TDataType[], useMock: boolean, resourceDictionaryBuilder: { (id: number): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
+		(endpoint: string, mockData: TDataType[], useMock: boolean, resourceDictionaryBuilder: { (id: number): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
 	createParentResource<TDataType extends IBaseDomainObject, TResourceDictionaryType>
-		($http: angular.IHttpService, endpoint: string, mockData: TDataType[], useMock: boolean, resourceDictionaryBuilder: { (id: number): TResourceDictionaryType }): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
+		(endpoint: string, mockData: TDataType[], useMock: boolean, resourceDictionaryBuilder: { (id: number): TResourceDictionaryType }): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
 }
 
 export class BaseResourceBuilder implements IBaseResourceBuilder {
-	createResource<TDataType extends IBaseDomainObject, TSearchParams>($http: angular.IHttpService, endpoint: string, mockData: TDataType[], useMock: boolean): IBaseDataService<TDataType, TSearchParams> {
-		return new BaseDataService($http, endpoint, mockData, useMock);
+	static $inject: string[] = ['$http', '$q'];
+	constructor(private $http: angular.IHttpService, private $q: angular.IQService) { }
+
+	createResource<TDataType extends IBaseDomainObject, TSearchParams>(endpoint: string, mockData: TDataType[], useMock: boolean): IBaseDataService<TDataType, TSearchParams> {
+		return new BaseDataService(this.$http, endpoint, mockData, useMock);
 	}
 
 	createParentResource<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
-		($http: angular.IHttpService, endpoint: string, mockData: TDataType[], useMock: boolean, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType> {
-		return new BaseParentDataService($http, endpoint, mockData, useMock, resourceDictionaryBuilder);
+		(endpoint: string, mockData: TDataType[], useMock: boolean, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType> {
+		return new BaseParentDataService(this.$http, endpoint, mockData, useMock, resourceDictionaryBuilder);
 	}
 }
 
