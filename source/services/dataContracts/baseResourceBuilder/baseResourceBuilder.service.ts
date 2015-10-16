582b78556019b84ee6984605e9f217d4a2cb6d0d
SamGraber <sgraber@renovo1.com>
2015-10-16 12:28:14 -0400

    The resource builder requires angular $http to build the resource

--- source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
+++ source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
@@ -1,11 +1,15 @@
+'use strict';
+
+import * as angular from 'angular';
+
 import { IBaseDataService, IBaseDomainObject } from '../baseDataService/baseData.service';
 import { IBaseParentDataService } from '../baseParentDataService/baseParentData.service';
 
 export interface IBaseResourceBuilder {
-	createResource<TDataType extends IBaseDomainObject, TSearchParams>(endpoint: string, mockData: any): IBaseDataService<TDataType, TSearchParams>;
-	createResource<TDataType extends IBaseDomainObject>(endpoint: string, mockData: any): IBaseDataService<TDataType, void>;
+	createResource<TDataType extends IBaseDomainObject, TSearchParams>($http: angular.IHttpService, endpoint: string, mockData: any): IBaseDataService<TDataType, TSearchParams>;
+	createResource<TDataType extends IBaseDomainObject>($http: angular.IHttpService, endpoint: string, mockData: any): IBaseDataService<TDataType, void>;
 	createParentResource<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
-		(endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
+		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
 	createParentResource<TDataType extends IBaseDomainObject, TResourceDictionaryType>
-		(endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
+		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
 }
