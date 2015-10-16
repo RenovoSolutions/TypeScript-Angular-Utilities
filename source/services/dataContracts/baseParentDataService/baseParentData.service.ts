5e1ad04bfd2ee68d0b283ca9322daec87e5ec64c
SamGraber <sgraber@renovo1.com>
2015-10-16 13:10:03 -0400

    Initial implementation of a baseParentData service

--- source/services/dataContracts/baseParentDataService/baseParentData.service.ts
+++ source/services/dataContracts/baseParentDataService/baseParentData.service.ts
@@ -1,8 +1,19 @@
 import * as ng from 'angular';
 
-import { IBaseDataService, IBaseDomainObject } from '../baseDataService/baseData.service';
+import { IBaseDataService, BaseDataService, IBaseDomainObject } from '../baseDataService/baseData.service';
 
 export interface IBaseParentDataService<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
 	extends IBaseDataService<TDataType, TSearchParams>{
 	childContracts(id: number): TResourceDictionaryType;
+}
+
+export class BaseParentDataService<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
+	extends BaseDataService<TDataType, TSearchParams> implements IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType> {
+	constructor($http: ng.IHttpService, endpoint: string, private resourceDictionaryBuilder: {(id: number): TResourceDictionaryType}) {
+		super($http, endpoint);
+	}
+
+	childContracts(id: number): TResourceDictionaryType {
+		return this.resourceDictionaryBuilder(id);
+	}
 }
