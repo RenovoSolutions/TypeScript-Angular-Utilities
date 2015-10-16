7f5904ce2b9e5ee1ecb9643e02c4a802040e0cfc
SamGraber <sgraber@renovo1.com>
2015-10-16 13:09:48 -0400

    Initial implementatino of a baseResourceBuilder service

--- source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
+++ source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
@@ -2,14 +2,31 @@
 
 import * as angular from 'angular';
 
-import { IBaseDataService, IBaseDomainObject } from '../baseDataService/baseData.service';
+import { IBaseDataService, BaseDataService, IBaseDomainObject } from '../baseDataService/baseData.service';
 import { IBaseParentDataService } from '../baseParentDataService/baseParentData.service';
+
+export var moduleName: string = 'rl.utilities.services.baseResourceBuilder';
+export var serviceName: string = 'baseResourceBuilder';
 
 export interface IBaseResourceBuilder {
 	createResource<TDataType extends IBaseDomainObject, TSearchParams>($http: angular.IHttpService, endpoint: string, mockData: any): IBaseDataService<TDataType, TSearchParams>;
 	createResource<TDataType extends IBaseDomainObject>($http: angular.IHttpService, endpoint: string, mockData: any): IBaseDataService<TDataType, void>;
 	createParentResource<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
-		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
+		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (id: number): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
 	createParentResource<TDataType extends IBaseDomainObject, TResourceDictionaryType>
-		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
+		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (id: number): TResourceDictionaryType }): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
 }
+
+export class BaseResourceBuilder implements IBaseResourceBuilder {
+	createResource<TDataType extends IBaseDomainObject, TSearchParams>($http: angular.IHttpService, endpoint: string, mockData: any): IBaseDataService<TDataType, TSearchParams> {
+		return new BaseDataService($http, endpoint);
+	}
+
+	createParentResource<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
+		($http: angular.IHttpService, endpoint: string, mockData: any, resourceDictionaryBuilder: { (): TResourceDictionaryType }): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType> {
+		return null;
+	}
+}
+
+angular.module(moduleName, [])
+	.service(serviceName, BaseResourceBuilder);
