18b84cf54803b71b324f5b05a3fe57ad6abd1780
SamGraber <sgraber@renovo1.com>
2015-10-21 10:49:12 -0400

    Added an export for baseParentSingleton data service.

--- source/services/dataContracts/dataContracts.module.ts
+++ source/services/dataContracts/dataContracts.module.ts
@@ -11,6 +11,7 @@ export var moduleName: string = 'rl.utilities.services.dataContracts';
 export { IBaseDataService, IBaseDataServiceFactory, IBaseDomainObject, BaseDataService, factoryName as baseDataServiceFactoryName } from './baseDataService/baseData.service';
 export * from './baseParentDataService/baseParentData.service';
 export { IBaseSingletonDataService, IBaseSingletonDataServiceFactory, BaseSingletonDataService, factoryName as baseSingletonDataServiceFactoryName } from './baseSingletonDataService/baseSingletonData.service';
+export * from './baseParentSingletonDataService/baseParentSingletonData.service';
 export { IBaseResourceBuilder, serviceName as builderServiceName } from './baseResourceBuilder/baseResourceBuilder.service';
 
 angular.module(moduleName, [
