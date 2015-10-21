a5cc6f5fa1f3b511719fa7136e1d9d0f0d1decfe
SamGraber <sgraber@renovo1.com>
2015-10-21 09:01:02 -0400

    Added documentation to the baseResource builder.

--- source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
+++ source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
@@ -13,38 +13,95 @@ export var moduleName: string = 'rl.utilities.services.baseResourceBuilder';
 export var serviceName: string = 'baseResourceBuilder';
 
 export interface IBaseResourceParams<TDataType extends IBaseDomainObject> {
+	/**
+	* Url to hit with getList and create
+	* - extended with /id for getDetail, update, and delete
+	*/
 	endpoint?: string;
+
+	/**
+	* Example data set to be used for testing and prototyping instead of hitting the endpoint
+	*/
 	mockData?: TDataType[];
+
+	/**
+	* Flag for specifying if the data service should use the mock data or hit the actual endpoint
+	* defaults to true if endpoint is not defined
+	*/
 	useMock?: boolean;
+
+	/**
+	* Processes data coming back from the server
+	*/
 	transform?: ITransformFunction<TDataType>;
 }
 
 export interface IParentResourceParams<TDataType extends IBaseDomainObject, TResourceDictionaryType> extends IBaseResourceParams<TDataType> {
+	/**
+	* Function that builds a dictionary of child resources available through childContracts(id)
+	*/
 	resourceDictionaryBuilder?: { (id: number): TResourceDictionaryType };
 }
 
 export interface ISingletonResourceParams<TDataType> {
+	/**
+	* Url to hit with get and update
+	*/
 	endpoint?: string;
+
+	/**
+	* Example object to be used for testing and prototyping instead of hitting the endpoint
+	*/
 	mockData?: TDataType;
+
+	/**
+	* Flag for specifying if the data service should use the mock data or hit the actual endpoint
+	* defaults to true if endpoint is not defined
+	*/
 	useMock?: boolean;
+
+	/**
+	* Processes data coming back from the server
+	*/
 	transform?: ITransformFunction<TDataType>;
 }
 
 export interface IParentSingletonResourceParams<TDataType, TResourceDictionaryType> extends ISingletonResourceParams<TDataType> {
+	/**
+	* Function that builds a dictionary of child resources available through childContracts(id)
+	*/
 	resourceDictionaryBuilder?: { (id: number): TResourceDictionaryType };
 }
 
 export interface IBaseResourceBuilder {
+	/**
+	* Create a standard resource with getList, getDetail, create, update, delete
+	*/
 	createResource<TDataType extends IBaseDomainObject, TSearchParams>(options: IBaseResourceParams<TDataType>): IBaseDataService<TDataType, TSearchParams>;
+	/**
+	* Create a standard resource with getList, getDetail, create, update, delete
+	*/
 	createResource<TDataType extends IBaseDomainObject>(options: IBaseResourceParams<TDataType>): IBaseDataService<TDataType, void>;
 
+	/**
+	* Create a parent resource that extends the standard with child resources available through childContracts(id)
+	*/
 	createParentResource<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
 		(options: IParentResourceParams<TDataType, TResourceDictionaryType>): IBaseParentDataService<TDataType, TSearchParams, TResourceDictionaryType>;
+	/**
+	* Create a parent resource that extends the standard with child resources available through childContracts(id)
+	*/
 	createParentResource<TDataType extends IBaseDomainObject, TResourceDictionaryType>
 		(options: IParentResourceParams<TDataType, TResourceDictionaryType>): IBaseParentDataService<TDataType, void, TResourceDictionaryType>;
 
+	/**
+	* Create a singleton resource with get and update
+	*/
 	createSingletonResource<TDataType>(options: ISingletonResourceParams<TDataType>): IBaseSingletonDataService<TDataType>;
 
+	/**
+	* Create a parent singleton resource that extends the singleton with child resources available through childContracts(id)
+	*/
 	createParentSingletonResource<TDataType, TResourceDictionaryType>
 		(options: IParentSingletonResourceParams<TDataType, TResourceDictionaryType>): IBaseParentSingletonDataService<TDataType, TResourceDictionaryType>;
 }
