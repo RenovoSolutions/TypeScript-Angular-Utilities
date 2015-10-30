4d4e82526bf0ab078b8a8826e2239c1f64867fec
SamGraber <sgraber@renovo1.com>
2015-10-30 14:41:09 -0400

    Updated the contract library so the constructor just takes the base resource builder and calls a function on it to get the injectables. This saves a child class from having to specify the same dependencies every time.

--- source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
+++ source/services/dataContracts/baseResourceBuilder/baseResourceBuilder.service.ts
@@ -4,7 +4,7 @@ import * as angular from 'angular';
 
 import { IArrayUtility, serviceName as arrayServiceName, moduleName as arrayModuleName } from '../../array/array.service';
 
-import { IContractLibrary, ContractLibrary } from './contractLibrary';
+import { IContractLibrary, ContractLibrary, ILibraryServices } from './contractLibrary';
 import { IBaseDataService, BaseDataService, IBaseDomainObject, ITransformFunction } from '../baseDataService/baseData.service';
 import { IBaseParentDataService, BaseParentDataService } from '../baseParentDataService/baseParentData.service';
 import { IBaseSingletonDataService, BaseSingletonDataService } from '../baseSingletonDataService/baseSingletonData.service';
@@ -86,6 +86,11 @@ export interface IParentSingletonResourceParams<TDataType, TResourceDictionaryTy
 
 export interface IBaseResourceBuilder {
 	/**
+	* A helper to pass into the constructor when building a new contracts library
+	*/
+	getLibraryServices(): ILibraryServices;
+
+	/**
 	* Create a standard resource with getList, getDetail, create, update, delete
 	*/
 	createResource<TDataType extends IBaseDomainObject, TSearchParams>(options: IBaseResourceParams<TDataType>): IBaseDataService<TDataType, TSearchParams>;
@@ -124,8 +129,11 @@ export class BaseResourceBuilder implements IBaseResourceBuilder {
 			, private $rootScope: angular.IRootScopeService
 			, private array: IArrayUtility) { }
 
-	createLibrary(): IContractLibrary {
-		return new ContractLibrary(this.$q, this.$rootScope);
+	getLibraryServices(): ILibraryServices {
+		return {
+			$q: this.$q,
+			$rootScope: this.$rootScope,
+		};
 	}
 
 	createResource<TDataType extends IBaseDomainObject, TSearchParams>(options: IBaseResourceParams<TDataType>): IBaseDataService<TDataType, TSearchParams> {
--- source/services/dataContracts/baseResourceBuilder/contractLibrary.ts
+++ source/services/dataContracts/baseResourceBuilder/contractLibrary.ts
@@ -3,24 +3,52 @@
 'use strict';
 
 import * as ng from 'angular';
+import * as _ from 'lodash';
+
+import { IBaseResourceBuilder } from './baseResourceBuilder.service';
 
 export interface IContractLibrary {
 	// extend with custom interface specifying child resources
 
 	flush(): void;
 
-	mockGet(resource: any, actionName: string, data: any): Sinon.SinonSpy;
+	mockGet(resource: any, data: any): Sinon.SinonSpy;
+	mockGetList(resource: any, data: any): Sinon.SinonSpy;
+	mockGetDetail(resource: any, data: any): Sinon.SinonSpy;
+}
+
+export interface ILibraryServices {
+	$q: ng.IQService;
+	$rootScope: ng.IRootScopeService;
 }
 
 export class ContractLibrary implements IContractLibrary {
-	constructor(private $q: ng.IQService
-			, private $rootScope: ng.IRootScopeService) { }
+	private $q: ng.IQService;
+	private $rootScope: ng.IRootScopeService;
+
+	constructor(builder: IBaseResourceBuilder) {
+		let services: ILibraryServices = builder.getLibraryServices();
+		this.$q = services.$q;
+		this.$rootScope = services.$rootScope;
+	}
 
 	flush(): void {
 		this.$rootScope.$digest();
 	}
 
-	mockGet(resource: any, actionName: string, data: any): Sinon.SinonSpy {
+	mockGet(resource: any, data: any): Sinon.SinonSpy {
+		return this.baseMockGet(resource, 'get', data);
+	}
+
+	mockGetList(resource: any, data: any): Sinon.SinonSpy {
+		return this.baseMockGet(resource, 'getList', data);
+	}
+
+	mockGetDetail(resource: any, data: any): Sinon.SinonSpy {
+		return this.baseMockGet(resource, 'getDetail', data);
+	}
+
+	private baseMockGet(resource: any, actionName: string, data: any): Sinon.SinonSpy {
 		let sinonInstance: Sinon.SinonStatic = sinon || <any>{ spy: (func: any): any => { return func; } };
 		let func: Sinon.SinonSpy = sinonInstance.spy((): any => {
 			return this.$q.when(data);
