dc3c841662208dffc0366a9c90699ce9b56eb263
SamGraber <samgraber@sundialproperties.com>
2015-12-22 10:40:54 -0500

    Implemented error handler for utilities.

---
+++ source/services/errorHandler/errorHandler.service.tests.ts
@@ -0,0 +1,109 @@
+﻿/// <reference path='../../../typings/chai/chai.d.ts' />
+/// <reference path='../../../typings/mocha/mocha.d.ts' />
+/// <reference path='../../../typings/sinon/sinon.d.ts' />
+/// <reference path='../../../typings/chaiAssertions.d.ts' />
+
+'use strict';
+
+import * as angular from 'angular';
+import 'angular-mocks';
+
+import { services } from 'typescript-angular-utilities';
+import __test = services.test;
+
+import {
+	moduleName,
+	serviceName,
+	IErrorHandlerService,
+	IRejection,
+	HttpStatusCode,
+} from './errorHandler.service';
+
+interface IWindowMock {
+	location: string;
+}
+
+interface INotificationMock {
+	error: Sinon.SinonSpy;
+}
+
+describe('errorHandler', () => {
+	var errorHandler: IErrorHandlerService;
+	var $window: IWindowMock;
+	var notification: INotificationMock;
+
+	beforeEach(() => {
+		angular.mock.module(moduleName);
+
+		$window = {
+			location: null,
+		};
+
+		notification = {
+			error: sinon.spy(),
+		};
+
+		__test.angularFixture.mock({
+			$window: $window,
+			notification: notification,
+		});
+
+		var services: any = __test.angularFixture.inject(serviceName);
+		errorHandler = services[serviceName];
+	});
+
+	it('should redirect the user to the login page on an unauthorized error', (): void => {
+		var rejection: IRejection = {
+			status: HttpStatusCode.unauthorized
+		};
+
+		errorHandler.httpResponseError(rejection);
+
+		expect($window.location).to.equal('/login.aspx');
+	});
+
+	it('should show an error for insufficient permissions', (): void => {
+		var rejection: IRejection = {
+			status: HttpStatusCode.forbidden
+		};
+
+		errorHandler.httpResponseError(rejection);
+
+		sinon.assert.calledOnce(notification.error);
+		sinon.assert.calledWith(notification.error, 'You have insufficient permissions to perform this action');
+	});
+
+	it('should show an error for invalid url', (): void => {
+		var rejection: IRejection = {
+			status: HttpStatusCode.invalidUrl
+		};
+
+		errorHandler.httpResponseError(rejection);
+
+		sinon.assert.calledOnce(notification.error);
+		sinon.assert.calledWith(notification.error, 'Resource not found. This issue has been logged');
+	});
+
+	it('should show an error for timeout', (): void => {
+		var rejection: IRejection = {
+			status: HttpStatusCode.timeout
+		};
+
+		errorHandler.httpResponseError(rejection);
+
+		sinon.assert.calledOnce(notification.error);
+		sinon.assert.calledWith(notification.error, 'Request timed out. Check your network connection or contact your administrator for issues');
+	});
+
+	it('should show an error for system error', (): void => {
+		var rejection: IRejection = {
+			status: HttpStatusCode.internalServerError
+		};
+
+		errorHandler.httpResponseError(rejection);
+
+		sinon.assert.calledOnce(notification.error);
+		sinon.assert.calledWith(notification.error, 'The system has encountered an error. This issue has been logged.' +
+												' Please contact support if you are unable to complete critical tasks');
+	});
+});
---
+++ source/services/errorHandler/errorHandler.service.ts
@@ -0,0 +1,81 @@
+﻿'use strict';
+
+import * as ng from 'angular';
+
+import { services } from 'typescript-angular-utilities';
+import __notification = services.notification;
+
+export var moduleName: string = 'rl21.services.errorHandler';
+export var serviceName: string = 'errorHandler';
+
+export enum HttpStatusCode {
+	unauthorized = 401,
+	forbidden = 403,
+	invalidUrl = 404,
+	timeout = 408,
+	internalServerError = 500,
+}
+
+export interface IRejection {
+	status: HttpStatusCode;
+}
+
+export interface IErrorHandlerService {
+	httpResponseError(rejection: IRejection): void;
+}
+
+export class ErrorHandlerService implements IErrorHandlerService {
+	static $inject: string[] = ['$window', __notification.serviceName];
+	constructor(private $window: ng.IWindowService, private notification: __notification.INotificationService) { }
+
+	private loginUrl: Location = <any>'/login.aspx';
+
+	httpResponseError(rejection: IRejection): void {
+		switch (rejection.status) {
+			case HttpStatusCode.unauthorized:
+				this.loggedOutError();
+				break;
+			case HttpStatusCode.forbidden:
+				this.insufficientPermissionsError();
+				break;
+			case HttpStatusCode.invalidUrl:
+				this.invalidUrlError();
+				break;
+			case HttpStatusCode.timeout:
+				this.timeoutError();
+				break;
+			case HttpStatusCode.internalServerError:
+				this.systemError();
+				break;
+			default:
+				console.log('Http status' + rejection.status + ' not handled');
+				console.log('Response: ' + rejection);
+				break;
+		}
+	}
+
+	private loggedOutError(): void {
+		this.$window.location = this.loginUrl;
+	}
+
+	private insufficientPermissionsError(): void {
+		this.notification.error('You have insufficient permissions to perform this action');
+	}
+
+	private invalidUrlError(): void {
+		this.notification.error('Resource not found. This issue has been logged');
+	}
+
+	private timeoutError(): void {
+		this.notification.error('Request timed out. Check your network connection or contact your administrator for issues');
+		// retry
+	}
+
+	private systemError(): void {
+		this.notification.error('The system has encountered an error. This issue has been logged.' +
+								' Please contact support if you are unable to complete critical tasks');
+	}
+}
+
+angular.module(moduleName, [__notification.moduleName])
+	.service(serviceName, ErrorHandlerService);
