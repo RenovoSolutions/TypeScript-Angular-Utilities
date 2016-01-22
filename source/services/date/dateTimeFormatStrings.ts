f16ca6e5b4f6101d49fbe540ee35cf299d63ec8c
SamGraber <samgraber@sundialproperties.com>
2016-01-22 16:26:44 -0500

    Implemented the dateConverter

--- source/services/date/dateTimeFormatStrings.ts
+++ source/services/date/dateTimeFormatStrings.ts
@@ -3,12 +3,14 @@
 export var dateTimeFormatServiceName: string = 'dateTimeFormatStrings';
 
 export interface IDateFormatStrings {
+	isoFormat: string;
 	dateTimeFormat: string;
 	dateFormat: string;
 	timeFormat: string;
 }
 
 export var defaultFormats: IDateFormatStrings = {
+	isoFormat: 'YYYY-MM-DDTHH:mm:ss',
 	dateTimeFormat: 'M/D/YYYY h:mm A',
 	dateFormat: 'M/D/YYYY',
 	timeFormat: 'h:mmA',
