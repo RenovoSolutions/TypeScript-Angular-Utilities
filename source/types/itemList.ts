6ebb1561ab991c6372bf8d05f951aca2e6dd8431
SamGraber <samgraber@sundialproperties.com>
2016-01-22 16:46:53 -0500

    Implemented an itemList for building smart enums.

---
+++ source/types/itemList.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+import * as _ from 'lodash';
+
+export interface IItem {
+    value: number;
+    name: string;
+    display: string;
+}
+
+export interface IItemList<TItemType extends IItem> {
+	get(value: number | string): TItemType;
+	all(): TItemType[];
+}
+
+export class ItemList<TItemType extends IItem> {
+	private items: TItemType[];
+
+	setItems(items: TItemType[]): void {
+		this.items = items;
+	}
+
+	get(value: number | string): TItemType {
+		var predicate: { (item: TItemType): boolean };
+
+		if (typeof value === 'string') {
+			predicate = (item: TItemType): boolean => {
+				return (item.name === value);
+			};
+		} else {
+			predicate = (item: TItemType): boolean => {
+				return (item.value === value);
+			};
+		}
+
+		return _.find(this.items, predicate);
+	}
+
+	all(): TItemType[] {
+		return this.items;
+	}
+}
--- source/types/types.module.ts
+++ source/types/types.module.ts
@@ -1,3 +1,4 @@
 'use strict';
 
 export * from './compareResult';
+export * from './itemList';
