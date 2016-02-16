50cb847671d64eb35854662800f6d9831be3cdb4
SamGraber <samgraber@sundialproperties.com>
2016-02-16 14:19:06 -0500

    Added an interface for serializable filters. Filter should specify a serialize method that returns an object that represents the current filter value.

--- source/filters/filter.ts
+++ source/filters/filter.ts
@@ -4,6 +4,10 @@ export interface IFilterWithCounts extends IFilter {
 	updateOptionCounts<TItemType>(data: TItemType[]): void;
 }
 
+export interface ISerializableFilter extends IFilter {
+	serialize<TFilterData>(): TFilterData;
+}
+
 export interface IFilter {
 	type: string;
 	filter<TItemType>(item: TItemType): boolean;
