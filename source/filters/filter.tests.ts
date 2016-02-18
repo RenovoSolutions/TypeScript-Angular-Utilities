3bbeabb8b202a67232c480bb161908e6be457ef6
SamGraber <samgraber@sundialproperties.com>
2016-02-18 12:20:40 -0500

    Created a base serializable filter class.

---
+++ source/filters/filter.tests.ts
@@ -0,0 +1,44 @@
+import { SerializableFilter } from './filter';
+
+describe('serializable filter', (): void => {
+	filter: SerializableFilter<any>;
+
+	beforeEach((): void {
+		filter = new SerializableFilter();
+	});
+
+	it('should provide a default serialize that returns the filter directly', (): void => {
+		filter.type = 'myFilter';
+		let serializedValue: any = filter.serialize();
+		expect(serializedValue).to.be.not.empty;
+		expect(serializedValue.type).to.equal('myFilter');
+	});
+
+	it('should fire an event every time onChange is called by default', (): void => {
+		let onValueChange: Sinon.SinonSpy = sinon.spy();
+		filter.subscribe(onValueChange);
+
+		filter.onChange();
+
+		sinon.assert.calledOnce(onValueChange);
+	});
+
+	it('should deep compare the new seralized value with the old one if force is set to false', (): void => {
+		let obj1: any = { prop: '1234' };
+		let obj2: any = { prop: '5678' };
+		filter.serialize = (): any => { return obj1; };
+		// set the initial value
+		filter.onChange();
+		let onValueChange: Sinon.SinonSpy = sinon.spy();
+		filter.subscribe(onValueChange);
+
+		filter.onChange(false);
+
+		sinon.assert.notCalled(onValueChange);
+
+		filter.serialize = (): any => { return obj2; };
+		filter.onChange(false);
+
+		sinon.assert.calledOnce(onValueChange);
+	});
+});
--- source/filters/filter.ts
+++ source/filters/filter.ts
@@ -2,6 +2,8 @@
 
 import * as Rx from 'rx';
 
+import { objectUtility } from '../services/object/object.service';
+
 export interface IFilterWithCounts extends IFilter {
 	updateOptionCounts<TItemType>(data: TItemType[]): void;
 }
@@ -18,4 +20,35 @@ export interface IValueChangeCallback<TFilterData> {
 export interface IFilter {
 	type: string;
 	filter<TItemType>(item: TItemType): boolean;
+}
+
+export class SerializableFilter<TFilterValue> implements ISerializableFilter {
+	type: string;
+	protected subject: Rx.Subject;
+	private _value: TFilterValue;
+
+	constructor() {
+		this.subject = new Rx.Subject();
+	}
+
+	// override
+	filter(item: any): boolean {
+		return true;
+	}
+
+	serialize(): TFilterValue {
+		return <any>this;
+	}
+
+	subscribe(onValueChange: IValueChangeCallback<TFilterValue>): Rx.Subscriber {
+		return this.subject.subscribe(onValueChange);
+	}
+
+	protected onChange(force: boolean = true): void {
+		let newValue: TFilterValue = this.serialize();
+		if (force || !objectUtility.areEqual(newValue, this._value)) {
+			this._value = newValue;
+			this.subject.onNext(this._value);
+		}
+	}
 }
