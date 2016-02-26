d67685852bb79f3256219f73a26b095205e583d7
SamGraber <samgraber@sundialproperties.com>
2016-02-26 10:26:19 -0500

    Added a search action to the base data service behavior. Makes a POST request against the endpoint and expects to get the data back with a dataSet property. (Needs to know this for handling transforms)

--- source/services/dataContracts/baseDataServiceBehavior.ts
+++ source/services/dataContracts/baseDataServiceBehavior.ts
@@ -19,6 +19,11 @@ export interface IGetListOptions<TDataType> extends IRequestOptions {
     getMockData(): TDataType[];
 }
 
+export interface ISearchOptions<TDataType> extends IRequestOptions {
+    params: any;
+    getMockData(): TDataType[];
+}
+
 export interface IGetItemOptions<TDataType> extends IRequestOptions {
     getMockData(): TDataType;
 }
@@ -38,8 +43,13 @@ export interface IDeleteOptions<TDataType> extends IRequestOptions {
     removeMockData(data: TDataType): void;
 }
 
+export interface ISearchResult<TDataType> {
+	dataSet: TDataType[];
+}
+
 export interface IBaseDataServiceBehavior<TDataType> {
 	getList(options: IGetListOptions<TDataType>): angular.IPromise<TDataType[]>;
+	search<TResultType>(options: ISearchOptions<TDataType>): angular.IPromise<TResultType>;
     getItem(options: IGetItemOptions<TDataType>): angular.IPromise<TDataType>;
     create(options: ICreateOptions<TDataType>): angular.IPromise<TDataType>;
     update(options: IUpdateOptions<TDataType>): angular.IPromise<TDataType>;
@@ -64,11 +74,32 @@ export class BaseDataServiceBehavior<TDataType> implements IBaseDataServiceBehav
         return promise.then((data: TDataType[]): TDataType[] => {
 			data = this.applyTransform(data, this.transform, false);
             if (options.logRequests) {
-                this.log('getList', data, options.endpoint, options.useMock);
+                this.log('getList', null, data, options.endpoint, options.useMock);
             }
             return data;
         })
     }
+
+	search<TResultType extends ISearchResult<TDataType>>(options: ISearchOptions<TDataType>): angular.IPromise<TResultType> {
+		let promise: angular.IPromise<TResultType>;
+        if (options.useMock) {
+            promise = this.$q.when({
+				dataSet: options.getMockData(),
+			});
+        } else {
+            promise = this.$http.post(options.endpoint, options.params)
+                .then((response: angular.IHttpPromiseCallbackArg<TResultType>): TResultType => {
+                return response.data;
+            });
+        }
+        return promise.then((result: TResultType): TResultType => {
+			result.dataSet = this.applyTransform(result.dataSet, this.transform, false);
+            if (options.logRequests) {
+				this.log('search', options.params, result, options.endpoint, options.useMock);
+            }
+            return result;
+        })
+	}
 
     getItem(options: IGetItemOptions<TDataType>): angular.IPromise<TDataType> {
         let promise: angular.IPromise<TDataType>;
@@ -83,7 +114,7 @@ export class BaseDataServiceBehavior<TDataType> implements IBaseDataServiceBehav
         return promise.then((data: TDataType): TDataType => {
             data = this.applyTransform(data, this.transform, false);
             if (options.logRequests) {
-                this.log('get', data, options.endpoint, options.useMock);
+                this.log('get', null, data, options.endpoint, options.useMock);
             }
             return data;
         });
@@ -104,7 +135,7 @@ export class BaseDataServiceBehavior<TDataType> implements IBaseDataServiceBehav
         return promise.then((data: TDataType): TDataType => {
             data = this.applyTransform(data, this.transform, false);
             if (options.logRequests) {
-                this.log('create', data, options.endpoint, options.useMock);
+                this.log('create', options.domainObject, data, options.endpoint, options.useMock);
             }
             return data;
         });
@@ -125,7 +156,7 @@ export class BaseDataServiceBehavior<TDataType> implements IBaseDataServiceBehav
         return promise.then((data: TDataType): TDataType => {
             data = this.applyTransform(data, this.transform, false);
             if (options.logRequests) {
-                this.log('update', options.domainObject, options.endpoint, options.useMock);
+                this.log('update', options.domainObject, data, options.endpoint, options.useMock);
             }
             return data;
         });
@@ -141,16 +172,25 @@ export class BaseDataServiceBehavior<TDataType> implements IBaseDataServiceBehav
         }
         return promise.then((): void => {
             if (options.logRequests) {
-                this.log('delete', options.domainObject, options.endpoint, options.useMock);
+                this.log('delete', options.domainObject, null, options.endpoint, options.useMock);
             }
         });
     }
 
-    private log(requestName: string, data: any, endpoint: string, useMock: boolean): void {
+    private log(requestName: string, params: any, data: any, endpoint: string, useMock: boolean): void {
         let mockString = useMock ? 'Mocked ' : '';
         let endpointString = endpoint == null ? 'unspecified' : endpoint;
         console.log(mockString + requestName + ' for endpoint ' + endpointString + ':');
-        console.log(data);
+
+		if (params != null) {
+			console.log('params:');
+			console.log(params);
+		}
+
+		if (data != null) {
+			console.log('data:');
+			console.log(data);
+		}
     }
 
     applyTransform(data: any, transform: IConverter<any> | {[index: string]: IConverter<any>}, toServer: boolean): any {
