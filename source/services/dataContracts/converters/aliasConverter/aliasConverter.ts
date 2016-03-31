d9d23391689e0d30ad3a1362a930eac93d654e96
SamGraber <samgraber@sundialproperties.com>
2016-03-31 14:28:45 -0400

    Implemented aliasConverter.

--- source/services/dataContracts/converters/aliasConverter/aliasConverter.ts
+++ source/services/dataContracts/converters/aliasConverter/aliasConverter.ts
@@ -1,14 +1,32 @@
+import * as _ from 'lodash';
+
 import { IConverter } from '../converters';
 
 export class AliasConverter<TDataType> implements IConverter<TDataType> {
 	constructor(private alias: string
-			, private composedConverter: IConverter<TDataType>) { }
+			, private composedConverter?: IConverter<TDataType>) { }
 
 	fromServer(raw: any, parent: any): TDataType {
-		return null;
+		if (!_.has(parent, this.alias)) {
+			return null;
+		}
+
+		raw = parent[this.alias];
+
+		if (this.composedConverter != null) {
+			return this.composedConverter.fromServer(raw, parent);
+		}
+
+		return raw;
 	}
 
 	toServer(data: TDataType, parent: any): any {
+		if (this.composedConverter != null) {
+			data = this.composedConverter.toServer(data, parent);
+		}
 
+		parent[this.alias] = data;
+
+		return null;
 	}
 }
