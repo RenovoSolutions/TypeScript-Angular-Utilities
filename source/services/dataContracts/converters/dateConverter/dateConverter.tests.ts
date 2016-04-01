9c86de83f598318102b13a3d352dc4781db7d485
SamGraber <samgraber@sundialproperties.com>
2016-04-01 11:03:11 -0400

    Use 'let'

--- source/services/dataContracts/converters/dateConverter/dateConverter.tests.ts
+++ source/services/dataContracts/converters/dateConverter/dateConverter.tests.ts
@@ -4,12 +4,12 @@ import * as moment from 'moment';
 
 describe('dateConverter', (): void => {
 	it('should get the date from an ISO string', (): void => {
-		var expectedDate: Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
+		let expectedDate: Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
 		expect(dateConverter.fromServer('2015-11-24T20:12:00-05:00')).to.deep.equal(expectedDate);
 	});
 
 	it('should convert the date to an ISO string', (): void => {
-		var date: Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
+		let date: Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
 		expect(dateConverter.toServer(date)).to.equal('2015-11-24T20:12:00');
 	});
 });
