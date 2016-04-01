d05b6bf128fac869eead45515c4a766714626172
SamGraber <samgraber@sundialproperties.com>
2016-04-01 16:53:16 -0400

    Build

--- source/services/dataContracts/dataContractsHelper/dataContractsHelper.service.js
+++ source/services/dataContracts/dataContractsHelper/dataContractsHelper.service.js
@@ -4,13 +4,14 @@ var DataContractsHelper = (function () {
     }
     DataContractsHelper.prototype.versionEndpoint = function (endpoint, versionNumber) {
         var versionExpression = /\/v\d+\//;
+        var apiExpression = /\/api\//;
         var versionString = 'v' + versionNumber;
         var searchResult = endpoint.search(versionExpression);
         if (searchResult !== -1) {
             return endpoint.replace(versionExpression, '/' + versionString + '/');
         }
         else {
-            return endpoint.replace('api', 'api/' + versionString);
+            return endpoint.replace(apiExpression, '/api/' + versionString + '/');
         }
     };
     return DataContractsHelper;
