388efe1b26422ef378464678dc479b59aef2b12f
SamGraber <samgraber@sundialproperties.com>
2016-04-01 12:47:03 -0400

    Forgot to add the time zone to the string we're asserting against.

--- source/services/dataContracts/converters/dateConverter/dateConverter.tests.ts
+++ source/services/dataContracts/converters/dateConverter/dateConverter.tests.ts
@@ -10,7 +10,7 @@ describe('dateConverter', (): void => {
 
 	it('should convert the date to an ISO string', (): void => {
 		let date: Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
-		expect(dateConverter.toServer(date)).to.equal('2015-11-24T20:12:00');
+		expect(dateConverter.toServer(date)).to.equal('2015-11-24T20:12:00-05:00');
 	});
 
 	it('should handle nulls', (): void => {
