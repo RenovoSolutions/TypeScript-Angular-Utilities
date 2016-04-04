be518ffad62f911f0a6b4bf9c5c3b0e475c574c9
SamGraber <samgraber@sundialproperties.com>
2016-04-04 11:29:40 -0400

    Added timezones to the date converter tests.

--- source/services/dataContracts/converters/dateConverter/dateConverter.tests.ts
+++ source/services/dataContracts/converters/dateConverter/dateConverter.tests.ts
@@ -1,16 +1,18 @@
 import { dateConverter, defaultFormats } from './dateConverter';
+import { timezones } from '../../../timezone/timezone.enum';
 
 import * as moment from 'moment';
+import 'moment-timezone';
 
 describe('dateConverter', (): void => {
 	it('should get the date from an ISO string', (): void => {
-		let expectedDate: moment.Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
-		expect(dateConverter.fromServer('2015-11-24T20:12:00-05:00')).to.deep.equal(expectedDate);
+		let expectedDate: moment.Moment = moment('2015-11-24T20:12:00-06:00', defaultFormats.isoFormat).tz(timezones.CST.momentName);
+		expect(dateConverter.fromServer('2015-11-24T20:12:00-06:00')).to.deep.equal(expectedDate);
 	});
 
 	it('should convert the date to an ISO string', (): void => {
-		let date: moment.Moment = moment('2015-11-24T20:12:00-05:00', defaultFormats.isoFormat);
-		expect(dateConverter.toServer(date)).to.equal('2015-11-24T20:12:00-05:00');
+		let date: moment.Moment = moment('2015-11-24T20:12:00-06:00', defaultFormats.isoFormat).tz(timezones.CST.momentName);
+		expect(dateConverter.toServer(date)).to.equal('2015-11-24T20:12:00-06:00');
 	});
 
 	it('should handle nulls', (): void => {
