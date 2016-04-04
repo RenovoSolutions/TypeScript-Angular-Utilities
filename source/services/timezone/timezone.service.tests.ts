20b95c722a8629516c7e153c9061ee847f9a70bf
SamGraber <samgraber@sundialproperties.com>
2016-04-04 10:02:16 -0400

    Import moment timezone and use it to set the timezone for the purpose of the test. Eventually we'll want to declare some sort of 'global' timezone settings for the test, or find a way to pull in the timezones through moment.

--- source/services/timezone/timezone.service.tests.ts
+++ source/services/timezone/timezone.service.tests.ts
@@ -2,10 +2,15 @@ import { timezone } from './timezone.service';
 import { defaultFormats } from '../date/date.module';
 
 import * as moment from 'moment';
+import 'moment-timezone';
 
 describe('timezone', (): void => {
+	beforeEach((): void => {
+		moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
+	});
+
 	it('should return the timezone', (): void => {
-		let date: moment.Moment = moment('2016-4-1T12:00:00-08:00', defaultFormats.isoFormat);
-		expect(timezone.getTimezone(date)).to.equal('-08:00x');
+		let date: moment.Moment = moment('2016-4-1T12:00:00-07:00', defaultFormats.isoFormat).tz('America/Los_Angeles');
+		expect(timezone.getTimezone(date)).to.equal('-07:00');
 	});
 });
