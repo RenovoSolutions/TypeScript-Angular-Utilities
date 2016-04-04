0988c644a5e729345b839e6321846b72fe4da3d4
SamGraber <samgraber@sundialproperties.com>
2016-04-04 10:05:49 -0400

    Apparently we don't need to setup the timezone after all. Moment-timezone is already pulling it in.

--- source/services/timezone/timezone.service.tests.ts
+++ source/services/timezone/timezone.service.tests.ts
@@ -5,10 +5,6 @@ import * as moment from 'moment';
 import 'moment-timezone';
 
 describe('timezone', (): void => {
-	beforeEach((): void => {
-		moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
-	});
-
 	it('should return the timezone', (): void => {
 		let date: moment.Moment = moment('2016-4-1T12:00:00-07:00', defaultFormats.isoFormat).tz('America/Los_Angeles');
 		expect(timezone.getTimezone(date)).to.equal('-07:00');
