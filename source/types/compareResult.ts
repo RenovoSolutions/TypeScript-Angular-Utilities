a6227f50f611346355d693cf150edab91f65dd3b
SamGraber <samgraber@sundialproperties.com>
2016-04-07 14:26:06 -0400

    Added an invalid value for compare results. Value is null.

--- source/types/compareResult.ts
+++ source/types/compareResult.ts
@@ -4,10 +4,15 @@ export enum CompareResult {
 	greater = 1,
 	equal = 0,
 	less = -1,
+	invalid = null,
 }
 
 export function getCompareResult(num: number): CompareResult {
 	'use strict';
+	if (num == null) {
+		return CompareResult.invalid;
+	}
+
 	if (num === 0) {
 		return CompareResult.equal;
 	} else if (num > 0) {
