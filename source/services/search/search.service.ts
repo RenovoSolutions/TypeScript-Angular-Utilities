21d59bff430f71d3ef834969fb0e259d0b26ef7c
SamGraber <samgraber@sundialproperties.com>
2016-04-12 14:49:34 -0400

    Implemented the core search functionality.

--- source/services/search/search.service.ts
+++ source/services/search/search.service.ts
@@ -2,6 +2,9 @@
 
 import * as _ from 'lodash';
 
+import { objectUtility } from '../object/object.service';
+import { stringUtility } from '../string/string.service';
+
 export interface ISearchUtility {
 	search(object: any, search: string, caseSensitive?: boolean): boolean;
 	tokenizedSearch(object: any, search: string, caseSensitive?: boolean): boolean;
@@ -9,7 +12,23 @@ export interface ISearchUtility {
 
 class SearchUtility implements ISearchUtility {
 	search(object: any, search: string, caseSensitive?: boolean): boolean {
-		return true;
+		if (objectUtility.isNullOrEmpty(search)) {
+			return true;
+		}
+
+		if (_.isObject(object)) {
+			let values: any = _.values(object);
+			return _.some(values, (value: any): boolean => { return this.search(value, search, caseSensitive); });
+		} else {
+			let dataString: string = objectUtility.toString(object);
+
+			if (!caseSensitive) {
+				search = search.toLowerCase();
+				dataString = dataString.toLowerCase();
+			}
+
+			return stringUtility.contains(dataString, search);
+		}
 	}
 
 	tokenizedSearch(object: any, search: string, caseSensitive?: boolean): boolean {
