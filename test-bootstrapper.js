89012b381dd798b1c71f2579ed1db720566d907e
Josh Graber <jgraber@renovo1.com>
2016-04-13 11:55:43 -0400

    Update to the latest version of gulp-utilities which will bundle JS files now instead of using ts-loader.

--- test-bootstrapper.js
+++ test-bootstrapper.js
@@ -1,4 +1,4 @@
 // require all modules ending in ".tests.ts" from source and all subdirectories
 
-var testsContext = require.context('./source', true, /\.tests\.ts$/);
+var testsContext = require.context('./source', true, /\.tests\.js$/);
 testsContext.keys().forEach(testsContext);
