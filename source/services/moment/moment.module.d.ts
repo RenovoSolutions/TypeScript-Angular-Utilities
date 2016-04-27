77875cd7790cb1c42cd6bfcc09774ef6e277e4cb
theoriginaljosh <theoriginaljosh@gmail.com>
2016-04-27 15:52:07 -0400

    removing moment wrappper and baseNotifier, notificationTypes that are not longer needed

--- source/services/moment/moment.module.d.ts
+++
@@ -1,3 +0,0 @@
-export declare var moduleName: string;
-export declare var serviceName: string;
-export declare function momentWrapper(): void;
--- source/services/notification/notificationTypes.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export interface INotifier {
-	info(message: string): void;
-	warning(message: string): void;
-	error(message: string): void;
-	success(message: string): void;
-}
