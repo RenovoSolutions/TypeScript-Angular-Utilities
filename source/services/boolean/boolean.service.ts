ec58e3b2697bef69868b7a0f027d90a033de621a
Josh Graber <jgraber@renovo1.com>
2016-04-27 10:22:39 -0400

    Convert boolean utility to angular 2.

--- source/services/boolean/boolean.service.ts
+++ source/services/boolean/boolean.service.ts
@@ -1,19 +1,9 @@
-'use strict';
-
-import * as angular from 'angular';
-
-export var moduleName: string = 'rl.utilities.services.boolean';
-export var serviceName: string = 'booleanUtility';
-
 export interface IBooleanUtility {
 	toBool(object: any): boolean;
 }
 
-class BooleanUtility implements IBooleanUtility {
+export class BooleanUtility implements IBooleanUtility {
 	toBool(object: any): boolean {
 		return !!object;
 	}
 }
-
-angular.module(moduleName, [])
-	.service(serviceName, BooleanUtility);
