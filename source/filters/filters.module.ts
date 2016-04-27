6896bdd44b29a9a104f550ac6d583520b02122d7
Josh Graber <jgraber@renovo1.com>
2016-04-27 13:44:27 -0400

    Removed the filters angular module.

--- source/filters/filters.module.ts
+++ source/filters/filters.module.ts
@@ -1,14 +1,5 @@
-import * as angular from 'angular';
-
 import * as isEmpty from './isEmpty/isEmpty';
 import * as truncate from './truncate/truncate';
 
 export { isEmpty, truncate };
 export * from './filter';
-
-export var name: string = 'rl.utilities.filters';
-
-angular.module(name, [
-	isEmpty.moduleName,
-	truncate.moduleName,
-]);
