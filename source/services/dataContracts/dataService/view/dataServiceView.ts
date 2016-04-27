9ca19eeece2a077ca3022b9db352ab3ea352bd68
SamGraber <samgraber@sundialproperties.com>
2016-04-27 15:49:55 -0400

    Converte dataServiceView to angular 2.

--- source/services/dataContracts/dataService/view/dataServiceView.ts
+++ source/services/dataContracts/dataService/view/dataServiceView.ts
@@ -1,6 +1,5 @@
-'use strict';
-
-import { IArrayUtility, serviceName as arrayServiceName, moduleName as arrayModuleName } from '../../../array/array.service';
+import { IArrayUtility } from '../../../array/array.service';
+import { IHttpUtility } from '../../../http/http.service';
 
 import { IConverter } from '../../converters/converters';
 import { IDataService, DataService, IBaseDomainObject } from '../data.service';
@@ -13,28 +12,23 @@ export interface IDataServiceView<TDataType extends IBaseDomainObject, TSearchPa
 	AsSingleton(parentId: number): ISingletonDataService<TDataType>;
 }
 
-// deprecated - use IDataServiceView
-export interface IBaseDataServiceView<TDataType extends IBaseDomainObject, TSearchParams> extends IDataServiceView<TDataType, TSearchParams> { }
-
 export interface IParentDataServiceView<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType>
-	extends IParentDataService<TDataType, TSearchParams, TResourceDictionaryType>{
+	extends IParentDataService<TDataType, TSearchParams, TResourceDictionaryType> {
 	AsSingleton(parentId: number): IParentSingletonDataService<TDataType, TResourceDictionaryType>;
 }
-
-// deprecated - use IParentDataServiceView
-export interface IBaseParentDataServiceView<TDataType extends IBaseDomainObject, TSearchParams, TResourceDictionaryType> extends IParentDataServiceView<TDataType, TSearchParams, TResourceDictionaryType> { }
 
 export class DataServiceView<TDataType extends IBaseDomainObject, TSearchParams>
 	extends DataService<TDataType, TSearchParams>
 	implements IDataServiceView<TDataType, TSearchParams> {
 
+	private http: IHttpUtility;
 	private transform: IConverter<TDataType> | { [index: string]: IConverter<any> };
 
-	constructor(private $http: angular.IHttpService
-            , private $q: angular.IQService
-            , array: IArrayUtility
+	constructor(http: IHttpUtility
+			, array: IArrayUtility
 			, options: IBaseResourceParams<TDataType>) {
-		super($http, $q, array, options);
+		super(http, array, options);
+		this.http = http;
 		this.transform = options.transform;
 	}
 
@@ -42,7 +36,7 @@ export class DataServiceView<TDataType extends IBaseDomainObject, TSearchParams>
 		let mockData: TDataType = _.find(this.mockData, (item: TDataType): boolean => {
 			return item.id === parentId;
 		});
-		let singleton: SingletonDataService<TDataType> = new SingletonDataService<TDataType>(this.$http, this.$q, {
+		let singleton: SingletonDataService<TDataType> = new SingletonDataService<TDataType>(this.http, {
 			endpoint: this.endpoint,
 			mockData: mockData,
 			transform: this.transform,
@@ -58,20 +52,21 @@ export class ParentDataServiceView<TDataType extends IBaseDomainObject, TSearchP
 	extends ParentDataService<TDataType, TSearchParams, TResourceDictionaryType>
 	implements IParentDataServiceView<TDataType, TSearchParams, TResourceDictionaryType> {
 
+	private http: IHttpUtility;
 	private transform: IConverter<TDataType> | { [index: string]: IConverter<any> };
 
-	constructor(private $http: angular.IHttpService
-            , private $q: angular.IQService
-            , array: IArrayUtility
-			, options: IParentResourceParams<TDataType, TResourceDictionaryType>) {
-		super($http, $q, array, options);
+	constructor(http: IHttpUtility
+		, array: IArrayUtility
+		, options: IParentResourceParams<TDataType, TResourceDictionaryType>) {
+		super(http, array, options);
+		this.http = http;
 	}
 
 	AsSingleton(parentId: number): IParentSingletonDataService<TDataType, TResourceDictionaryType> {
 		let mockData: TDataType = _.find(this.mockData, (item: TDataType): boolean => {
 			return item.id === parentId;
 		});
-		let singleton: ParentSingletonDataService<TDataType, TResourceDictionaryType> = new ParentSingletonDataService<TDataType, TResourceDictionaryType>(this.$http, this.$q, {
+		let singleton: ParentSingletonDataService<TDataType, TResourceDictionaryType> = new ParentSingletonDataService<TDataType, TResourceDictionaryType>(this.http, {
 			endpoint: this.endpoint,
 			mockData: mockData,
 			resourceDictionaryBuilder: this.resourceDictionaryBuilder,
