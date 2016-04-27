d7cf77949ff26fefebe6174bbdbddd260ff0372d
Josh Graber <jgraber@renovo1.com>
2016-04-27 16:46:48 -0400

    Include polyfills and shims required for testing.

--- test-bootstrapper.js
+++ test-bootstrapper.js
@@ -1,3 +1,6 @@
+require('core-js/es6');
+require('core-js/es7/reflect');
+
 // require all modules ending in ".tests.ts" from source and all subdirectories
 
 var testsContext = require.context('./source', true, /\.tests\.js$/);
