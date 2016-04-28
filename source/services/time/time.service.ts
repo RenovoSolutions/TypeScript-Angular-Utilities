f702014de1005490ab4e698b1089ef76f1d0cb04
SamGraber <samgraber@sundialproperties.com>
2016-04-28 10:18:09 -0400

    Converted the time service to angular 2.

--- source/services/time/time.service.ts
+++ source/services/time/time.service.ts
@@ -1,13 +1,8 @@
-'use strict';
-
-import * as angular from 'angular';
+import { OpaqueToken, Provider } from 'angular2/core';
 import * as moment from 'moment';
 
 import { CompareResult } from '../../types/compareResult';
 import { defaultFormats } from '../date/date.module';
-
-export var moduleName: string = 'rl.utilities.services.time';
-export var serviceName: string = 'timeUtility';
 
 export interface ITimeUtility {
 	compareTimes(time1: string, time2: string): CompareResult;
@@ -34,5 +29,8 @@ export class TimeUtility {
 
 export let timeUtility: ITimeUtility = new TimeUtility();
 
-angular.module(moduleName, [])
-	.service(serviceName, TimeUtility);
+export const timeToken: OpaqueToken = new OpaqueToken('A service for comparing times');
+
+export const TIME_PROVIDER: Provider = new Provider(timeToken, {
+	useClass: TimeUtility,
+});
