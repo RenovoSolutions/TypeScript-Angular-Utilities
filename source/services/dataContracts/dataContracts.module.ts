e7bb102ce2fa5bf08fa858f7d8b80a44e12297fd
Josh Graber <jgraber@renovo1.com>
2016-04-28 11:28:56 -0400

    Export all data contracts providers together.

--- source/services/dataContracts/dataContracts.module.ts
+++ source/services/dataContracts/dataContracts.module.ts
@@ -1,3 +1,9 @@
+import { Provider } from 'angular2/core';
+
+import { RESOURCE_BUILDER_PROVIDER } from './resourceBuilder/resourceBuilder.service';
+import { DATA_SERVICE_PROVIDER } from './dataService/data.service';
+import { SINGLETON_DATA_SERVICE_PROVIDER } from './singletonDataService/singletonData.service';
+
 import * as converters from './converters/converters';
 import * as mocks from './contractLibrary/dataServiceMocks';
 
@@ -9,3 +15,9 @@ export * from './singletonDataService/singletonData.service';
 export * from './singletonDataService/parent/parentSingletonData.service';
 export * from './resourceBuilder/resourceBuilder.service';
 export { converters, mocks };
+
+export const DATA_CONTRACT_PROVIDERS: Provider[] = [
+	RESOURCE_BUILDER_PROVIDER,
+	DATA_SERVICE_PROVIDER,
+	SINGLETON_DATA_SERVICE_PROVIDER,
+];
