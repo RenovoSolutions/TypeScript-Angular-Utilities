b8ebdb83016ebc1593a8720a3f11fa82616eeaaf
Josh Graber <jgraber@renovo1.com>
2016-05-13 10:26:31 -0400

    Remove the webpack configuration.

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-var webpack = require('gulp-utilities').webpack;
-var library = require('./webpack.library.json');
-
-var webpackConfig = webpack.library(library);
-
-webpackConfig.devtool = 'inline-source-map';
-
-module.exports = webpackConfig;
--- webpack.min.config.js
+++
@@ -1,6 +0,0 @@
-var webpack = require('gulp-utilities').webpack;
-var library = require('./webpack.library.json');
-
-var webpackConfig = webpack.libraryMin(library);
-
-module.exports = webpackConfig;
