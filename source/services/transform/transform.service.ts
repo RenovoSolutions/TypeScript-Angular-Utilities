cb74925ca3c5684069d2a63470f296bebeb9251a
SamGraber <samgraber@sundialproperties.com>
2016-05-26 12:17:49 -0400

    Added a type for transforms.

--- source/services/transform/transform.service.ts
+++ source/services/transform/transform.service.ts
@@ -1,12 +1,14 @@
 import { Provider, OpaqueToken } from '@angular/core';
 import * as _ from 'lodash';
 
+export type ITransform<TItemType, TReturnType> = string | { (item: TItemType): TReturnType };
+
 export interface ITransformService {
-	getValue<TItemType, TReturnType>(item: TItemType, transform: { (item: TItemType): TReturnType } | string);
+	getValue<TItemType, TReturnType>(item: TItemType, transform: ITransform<TItemType, TReturnType>);
 }
 
 export class TransformService implements ITransformService {
-	getValue<TItemType, TReturnType>(item: TItemType, transform: { (item: TItemType): TReturnType } | string) {
+	getValue<TItemType, TReturnType>(item: TItemType, transform: ITransform<TItemType, TReturnType>) {
 		if (item == null) {
 			return null;
 		}
