9d1a2634bffd49ab8fcbd8e47eac353bbacbec9f
Josh Graber <jgraber@renovo1.com>
2016-05-26 10:03:11 -0400

    Add wipe-npm task
    Add a simple task for wiping the node_modules folder. Sometimes npm is a little flaky, so this is convenient for wiping the folder and starting over.

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,10 @@
 var utilities = require('gulp-utilities');
+var gulp = require('gulp');
+var del = require('del');
+
+var utilities = require('@renovolive/gulp-utilities');
 utilities.gulp.clean.config();
+
+gulp.task('wipe-npm', () => {
+	return del('node_modules');
+});
