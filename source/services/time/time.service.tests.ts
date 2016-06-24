93e29cbcab76ef7aaff470ca82a39ba3a6141e64
SamGraber <samgraber@sundialproperties.com>
2016-06-24 16:00:54 -0400

    Added the ability to parse to and from a time object model
    For our new time input, we'll want to pull apart the data model into an ITime object for display and manipulation reasons. It will be nice to have simple utility functions for parsing times from a string and formating back. That way this logic is pulled into one place.

--- source/services/time/time.service.tests.ts
+++ source/services/time/time.service.tests.ts
@@ -1,16 +1,36 @@
 import { ITimeUtility, TimeUtility } from './time.service';
 import { CompareResult } from '../../types/compareResult';
+import { stringUtility } from '../string/string.service';
 
 describe('timeUtility', () => {
 	let timeUtility: ITimeUtility;
 
 	beforeEach(() => {
-		timeUtility = new TimeUtility();
+		timeUtility = new TimeUtility(stringUtility);
 	});
 
 	it('should compare times and return a compare result to indicate which is greater', (): void => {
 		expect(timeUtility.compareTimes('12:00PM', '1:00PM')).to.equal(CompareResult.less);
 		expect(timeUtility.compareTimes('12:00PM', '12:00PM')).to.equal(CompareResult.equal);
 		expect(timeUtility.compareTimes('2:00PM', '1:00PM')).to.equal(CompareResult.greater);
+	});
+
+	it('should return null if the time is null', (): void => {
+		expect(timeUtility.parseTime(null)).to.be.null;
+		expect(timeUtility.formatTime(null)).to.be.null;
+	});
+
+	it('should parse the time string into a time object', (): void => {
+		expect(timeUtility.parseTime('8:00AM')).to.deep.equal({ hour: 8, minute: 0, period: 'AM' });
+		expect(timeUtility.parseTime('12:00PM')).to.deep.equal({ hour: 12, minute: 0, period: 'PM' });
+		expect(timeUtility.parseTime('10:15AM')).to.deep.equal({ hour: 10, minute: 15, period: 'AM' });
+		expect(timeUtility.parseTime('1:41PM')).to.deep.equal({ hour: 1, minute: 41, period: 'PM' });
+	});
+
+	it('should format the time object into a time string', (): void => {
+		expect(timeUtility.formatTime({ hour: 8, minute: 0, period: 'AM' })).to.equal('8:00AM');
+		expect(timeUtility.formatTime({ hour: 12, minute: 0, period: 'PM' })).to.equal('12:00PM');
+		expect(timeUtility.formatTime({ hour: 10, minute: 15, period: 'AM' })).to.equal('10:15AM');
+		expect(timeUtility.formatTime({ hour: 1, minute: 41, period: 'PM' })).to.equal('1:41PM');
 	});
 });
--- source/services/time/time.service.ts
+++ source/services/time/time.service.ts
@@ -1,14 +1,41 @@
-import { OpaqueToken, Provider } from '@angular/core';
+import { OpaqueToken, Provider, Inject, Injectable } from '@angular/core';
 import * as moment from 'moment';
+import { takeRight, dropRight, padStart } from 'lodash';
 
 import { CompareResult } from '../../types/compareResult';
 import { defaultFormats } from '../date/date.module';
+import { IStringUtility, stringToken, stringUtility } from '../string/string.service';
+
+export interface ITime {
+	hour: number;
+	minute: number;
+	period: string;
+}
+
+export interface ITimePeriods {
+	AM: string;
+	PM: string;
+}
+
+export const timePeriods: ITimePeriods = {
+	AM: 'AM',
+	PM: 'PM',
+};
 
 export interface ITimeUtility {
 	compareTimes(time1: string, time2: string): CompareResult;
+	parseTime(value: string): ITime;
+	formatTime(time: ITime): string;
 }
 
+@Injectable()
 export class TimeUtility {
+	stringUtility: IStringUtility;
+
+	constructor( @Inject(stringToken) stringUtility: IStringUtility) {
+		this.stringUtility = stringUtility;
+	}
+
 	compareTimes(time1: string, time2: string): CompareResult {
 		let format: string = defaultFormats.timeFormat;
 
@@ -25,9 +52,30 @@ export class TimeUtility {
 			return CompareResult.less;
 		}
 	}
+
+	parseTime(value: string): ITime {
+		if (value == null) {
+			return null;
+		}
+
+		let time: ITime = <any>{};
+		let [hourString, subsetWithoutHour]: string[] = value.split(':');
+		time.hour = this.stringUtility.toNumber(hourString);
+		time.minute = this.stringUtility.toNumber(dropRight(subsetWithoutHour, 2).join(''));
+		time.period = takeRight(subsetWithoutHour, 2).join('') === timePeriods.PM
+					? timePeriods.PM
+					: timePeriods.AM;
+		return time;
+	}
+
+	formatTime(time: ITime): string {
+		return time != null
+			? time.hour + ':' + padStart(time.minute.toString(), 2, '0') + time.period
+			: null;
+	}
 }
 
-export let timeUtility: ITimeUtility = new TimeUtility();
+export let timeUtility: ITimeUtility = new TimeUtility(stringUtility);
 
 export const timeToken: OpaqueToken = new OpaqueToken('A utility for working with time');
 
