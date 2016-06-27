c70dfb8d1cc752bb4f9d611999f10f73aae0efea
SamGraber <samgraber@sundialproperties.com>
2016-06-27 08:00:15 -0400

    Also test midnight
    This was a suggestion by Steven to hit all the possible edge cases.

--- source/services/time/time.service.tests.ts
+++ source/services/time/time.service.tests.ts
@@ -25,6 +25,7 @@ describe('timeUtility', () => {
 	it('should parse the time string into a time object', (): void => {
 		expect(timeUtility.parseTime('8:00AM')).to.deep.equal({ hour: 8, minute: 0, period: 'AM' });
 		expect(timeUtility.parseTime('12:00PM')).to.deep.equal({ hour: 12, minute: 0, period: 'PM' });
+		expect(timeUtility.parseTime('12:00AM')).to.deep.equal({ hour: 12, minute: 0, period: 'AM' });
 		expect(timeUtility.parseTime('10:15AM')).to.deep.equal({ hour: 10, minute: 15, period: 'AM' });
 		expect(timeUtility.parseTime('1:41PM')).to.deep.equal({ hour: 1, minute: 41, period: 'PM' });
 	});
@@ -32,6 +33,7 @@ describe('timeUtility', () => {
 	it('should format the time object into a time string', (): void => {
 		expect(timeUtility.formatTime({ hour: 8, minute: 0, period: 'AM' })).to.equal('8:00AM');
 		expect(timeUtility.formatTime({ hour: 12, minute: 0, period: 'PM' })).to.equal('12:00PM');
+		expect(timeUtility.formatTime({ hour: 12, minute: 0, period: 'AM' })).to.equal('12:00AM');
 		expect(timeUtility.formatTime({ hour: 10, minute: 15, period: 'AM' })).to.equal('10:15AM');
 		expect(timeUtility.formatTime({ hour: 1, minute: 41, period: 'PM' })).to.equal('1:41PM');
 	});
