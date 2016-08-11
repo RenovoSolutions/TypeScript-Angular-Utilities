bc01c4075c4cafbe9a97b0a6d736c435a0ffc176
theoriginaljosh <theoriginaljosh@gmail.com>
2016-08-11 08:49:06 -0400

    Email validation service
    very simple service that takes a string and returns true or false that it is in a valid email format.

---
+++ source/services/validation/emailValidation.service.tests.ts
@@ -0,0 +1,98 @@
+
+import {EmailValidationService} from './emailValidation.service';
+
+describe.only('EmailValidationService', () => {
+	let emailValidationService = new EmailValidationService();
+
+	const validCharacterTestCases: string[] = ['_', '+', '.', '+'];
+	const invalidCharacterTestCases: string[] = ['!', '^', '%', '*', ' ', '~', '&', '$', '#', '?', '`', '=', '/', '\\', ',', ':', ';', '"', '\'', '{', '}', '[', ']', '(', ')', '|', '>', '<'];
+	const invalidStartCharacterTestCase: string[] = ['@', '.'];
+
+	it('should return true when email is valid', () => {
+		const fakeEmailToAdd = 'fake@email.com';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.true;
+	});
+
+	it('should return true when email is valid', () => {
+		const fakeEmailToAdd = 'fake@@email.com';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.false;
+	});
+
+
+	invalidCharacterTestCases.forEach((invalidCharacter): void => {
+		it(`should return false when an emailaddress contains ${invalidCharacter}`, () => {
+			const fakeEmailToAdd = `fake${invalidCharacter}invalid@email.org`;
+
+			var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+			expect(isValid).to.be.false;
+		});
+	});
+
+	validCharacterTestCases.forEach((invalidCharacter): void => {
+		it(`should return true when an emailaddress contains ${invalidCharacter}`, () => {
+			const fakeEmailToAdd = `fake${invalidCharacter}invalid@email.org`;
+
+			var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+			expect(isValid).to.be.true;
+		});
+	});
+
+	invalidStartCharacterTestCase.forEach((invalidCharacter): void => {
+		it(`should return false when an emailaddress starts with ${invalidCharacter}`, () => {
+			const fakeEmailToAdd = `${invalidCharacter}invalid@email.org`;
+
+			var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+			expect(isValid).to.be.false;
+		});
+	});
+
+	it('should return false when an emailaddress ends with .', () => {
+		const fakeEmailToAdd = 'invalid@emailorg.';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.false;
+	});
+
+	it('should return false when an emailaddress ends with', () => {
+		const fakeEmailToAdd = 'invalidemail.org@';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.false;
+	});
+
+	it('should not contain a double dot', () => {
+		const fakeEmailToAdd = 'invalid@email..org@';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.false;
+	});
+
+	it('should allow more than one dot contiguously', () => {
+		const fakeEmailToAdd = 'fake@cs.email.org';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.true;
+	});
+
+	it('should not allow a dot immediately after an at sign', () => {
+		const fakeEmailToAdd = 'fake@.email.org';
+
+		var isValid = emailValidationService.isValidEmailAddress(fakeEmailToAdd);
+
+		expect(isValid).to.be.false;
+	});
+});
+
