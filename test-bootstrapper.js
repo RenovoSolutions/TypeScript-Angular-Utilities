650cf813e0caf5e37fdc3b0e2015025d99488c16
SamGraber <authorsamgraber@gmail.com>
2016-09-23 13:36:31 -0400

    Drop unused test-bootstrapper

--- test-bootstrapper.js
+++
@@ -1,7 +0,0 @@
-require('core-js/es6');
-require('core-js/es7/reflect');
-
-// require all modules ending in ".tests.ts" from source and all subdirectories
-
-var testsContext = require.context('./source', true, /\.tests\.js$/);
-testsContext.keys().forEach(testsContext);
