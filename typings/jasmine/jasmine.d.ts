1dc9be5992b921da3749784340b18cf62932f813
jbowen015 <jbowen@renovo1.com>
2017-02-23 11:12:14 -0500

    Add focus and exclude functions to jasmine typings

--- typings/jasmine/jasmine.d.ts
+++ typings/jasmine/jasmine.d.ts
@@ -1,4 +1,8 @@
 declare var describe: { (name: string, specs: { (): any }) };
 declare var it: { (name: string, spec: { (done?: Function): any }) };
+declare var fdescribe: { (name: string, specs: { (): any }) };
+declare var fit: { (name: string, spec: { (done?: Function): any }) };
+declare var xdescribe: { (name: string, specs: { (): any }) };
+declare var xit: { (name: string, spec: { (done?: Function): any }) };
 declare var beforeEach: { (setup: { (): any }) };
 declare var afterEach: { (cleanup: { (): any }) };
